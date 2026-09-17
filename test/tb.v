9729469694d4872f75a888ea8a3e60df5fba4c75
SebNavaUrri <snavaurr@uwaterloo.ca>
2026-09-17 19:43:05 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -14,14 +14,13 @@ module tb ();
   end
 
   // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    input  wire [7:0] load_bits,    // Dedicated inputs
+    input wire  load_en,   // Dedicated outputs
+    input wire out,
+    input  wire en,      // always 1 when the design is powered, so you can ignore it
+    input  wire clk,      // clock
+    input  wire rst,        // reset
+    output wire [7:0] out
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
