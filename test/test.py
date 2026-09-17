952d636d4a375f7a671b8617b09d0b11649e91aa
SebNavaUrri <snavaurr@uwaterloo.ca>
2026-09-17 19:57:25 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -16,10 +16,8 @@ async def test_project(dut):
 
     # Reset
     dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.load_en.value = 0
+    dut.out_en.value = 1
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
