812447299e25dc31bda357280422a6910195b958
SebNavaUrri <snavaurr@uwaterloo.ca>
2026-09-17 18:46:39 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,6 +17,7 @@ module tt_um_example (
 );
  
   reg [7:0] counter;
+    
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = counter;  // Example: ou_out is the sum of ui_in and uio_in
@@ -26,9 +27,11 @@ module tt_um_example (
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
+    always @(posedge clk or posedge rst_n or posedge load) begin
+        if (rst_n) begin
           counter <= 8'b0;
+        end else if(load) begin
+            counter <= ui_in;
         end else if (counter == 8'hff) begin
           counter <= 8'b0;
       end else begin
