71a1400452d1924f52a594f7cf6e1b48d297962c
SebNavaUrri <snavaurr@uwaterloo.ca>
2026-09-17 20:26:27 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -19,7 +19,7 @@ async def test_project(dut):
     dut.load_en.value = 0
     dut.out_en.value = 1
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    dut.rst_n.value = 0
 
     dut._log.info("Test project behavior")
 
