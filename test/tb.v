5aea9d62fb61e44de3b8bb47e465fac40a7707ee
SebNavaUrri <snavaurr@uwaterloo.ca>
2026-09-17 20:21:58 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -35,14 +35,13 @@ module tb ();
       .VGND(VGND),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+     .load_bits  (load_bits),    // Dedicated inputs
+     .load_en (load_en),   // Dedicated outputs
+     .out_en (out_en),   // IOs: Input path
+     .en (en),  // IOs: Output path
+     .rst (rst),   // IOs: Enable path (active high: 0=input, 1=output)
+     .out    (out),      // enable - goes high when design is selected
+      .clk    (clk)      // clock
   );
 
 endmodule
